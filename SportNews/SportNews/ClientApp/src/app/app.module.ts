3ddc05e24e77b61099118245dda670ddb3c6980b
DevMrSerJ <58230079+DevMrSerJ@users.noreply.github.com>
2022-02-28 13:23:33 +0400

    add pages

--- SportNews/SportNews/ClientApp/src/app/app.module.ts
+++ SportNews/SportNews/ClientApp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
@@ -7,6 +8,15 @@ import { PageArticleComponent } from './page-article/page-article.component';
 import { ClubsStatisticsComponent } from './clubs-statistics/clubs-statistics.component';
 import { CardArticleComponent } from './card-article/card-article.component';
 import { FooterComponent } from './footer/footer.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PageMainComponent } from './page-main/page-main.component';
+import { PageAuthorizationComponent } from './page-authorization/page-authorization.component';
+
+const appRoutes: Routes = [
+  { path: '', component: PageMainComponent },
+  { path: 'article', component: PageNotFoundComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
@@ -15,10 +25,14 @@ import { FooterComponent } from './footer/footer.component';
     PageArticleComponent,
     ClubsStatisticsComponent,
     CardArticleComponent,
-    FooterComponent
+    FooterComponent,
+    PageNotFoundComponent,
+    PageMainComponent,
+    PageAuthorizationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
