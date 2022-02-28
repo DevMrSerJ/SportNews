c1c607c3ccaca6514e4993a3841532ceb07024cf
DevMrSerJ <58230079+DevMrSerJ@users.noreply.github.com>
2022-02-28 12:23:15 +0400

    add cards with info about article

--- SportNews/SportNews/ClientApp/src/app/app.module.ts
+++ SportNews/SportNews/ClientApp/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { NavigationComponent } from './navigation/navigation.component';
 import { PageArticleComponent } from './page-article/page-article.component';
 import { ClubsStatisticsComponent } from './clubs-statistics/clubs-statistics.component';
 import { CardArticleComponent } from './card-article/card-article.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { CardArticleComponent } from './card-article/card-article.component';
     NavigationComponent,
     PageArticleComponent,
     ClubsStatisticsComponent,
-    CardArticleComponent
+    CardArticleComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
--- SportNews/SportNews/ClientApp/src/app/card-article/card-article.component.ts
+++ SportNews/SportNews/ClientApp/src/app/card-article/card-article.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Article {
+  id: string;
+  image: string;
+  header: string;
+  shortText: string;
+  text: string;
+  author: string;
+  date: string;
+  sport: string;
+}
+
 @Component({
   selector: 'app-card-article',
   templateUrl: './card-article.component.html',
@@ -7,9 +18,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardArticleComponent implements OnInit {
 
+  public articles: Article[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    this.articles.push(
+      {
+        id: "",
+        image: "",
+        header: "Леонид Слуцкий: «Арбитр точно не повлиял на ход матча»",
+        shortText: "Главный тренер «Рубина» Леонид Слуцкий прокомментировал ничью с «Уфой» (1:1) в матче 12-го тура Тинькофф РПЛ.",
+        text: "",
+        author: "Генич К.М.",
+        date: "24.10.2021",
+        sport: "футбол"
+      },
+      {
+        id: "",
+        image: "",
+        header: "Владимир Познер — о матче «Зенита» со «Спартаком»",
+        shortText: "Телеведущий и журналист Владимир Познер поделился ожиданиями от матча 12-го тура Тинькофф РПЛ между «Зенитом» и «Спартаком».",
+        text: "",
+        author: "Генич К.М.",
+        date: "24.10.2021",
+        sport: "футбол"
+      },
+      {
+        id: "",
+        image: "",
+        header: "Федор Чалов — о голе в ворота «Крыльев»",
+        shortText: "Форвард ЦСКА Федор Чалов подвел итог победы в матче 12-го тура Тинькофф РПЛ с «Крыльями Советов» (3:1).",
+        text: "",
+        author: "Генич К.М.",
+        date: "24.10.2021",
+        sport: "футбол"
+      }
+    );
   }
 
 }
