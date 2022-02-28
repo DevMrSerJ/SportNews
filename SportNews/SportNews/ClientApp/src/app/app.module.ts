97ca2b6dec2268357cec4c252ad5932d1fa67011
DevMrSerJ <58230079+DevMrSerJ@users.noreply.github.com>
2022-02-28 10:38:56 +0400

    Add navigation and page for article

--- SportNews/SportNews/ClientApp/src/app/app.module.ts
+++ SportNews/SportNews/ClientApp/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { PageArticleComponent } from './page-article/page-article.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    PageArticleComponent
   ],
   imports: [
     BrowserModule
