c502a34ed6b9b3a58fa77efd6e4f86d2907f06c0
DevMrSerJ <58230079+DevMrSerJ@users.noreply.github.com>
2022-03-14 16:37:12 +0400

    Add 2 methods for get articles

--- SportNews/SportNews/Controllers/ArticleController.cs
+++ SportNews/SportNews/Controllers/ArticleController.cs
@@ -33,11 +33,83 @@ namespace SportNews.Controllers
 									Id = article.Id,
 									Header = article.Header,
 									ShortText = article.ShortText,
-									Text = article.Text,
-									ImageUrl = article.ImageUrl,
 									Author = user.Surname + user.Name[0] + user.Patronymic[0],
 									DatePublish = article.DatePublish,
 									Sport = sport.Name
+								 }).ToList();
+
+					return users;
+				}
+			}
+			catch (Exception ex)
+			{
+				throw new Exception(ex.Message);
+			}
+		}
+
+		/// <summary>
+		/// Get full information about specific article.
+		/// </summary>
+		/// <param name="id">Id Articles.</param>
+		/// <returns>Full information about this article.</returns>
+		[HttpGet("{Id}")]
+		public IEnumerable<object> Get([FromRoute(Name = "Id")] Guid id)
+		{
+			try
+			{
+				using (SportNewsContext db = new SportNewsContext())
+				{
+					var users = (from article in db.Article
+								 join user in db.User on article.AuthorId equals user.Id
+								 join sport in db.TypeSport on article.TypeSportId equals sport.Id
+								 where article.Id == id
+								 select new
+								 {
+									 Id = article.Id,
+									 Header = article.Header,
+									 ShortText = article.ShortText,
+									 Text = article.Text,
+									 ImageUrl = article.ImageUrl,
+									 Author = user.Surname + user.Name[0] + user.Patronymic[0],
+									 DatePublish = article.DatePublish,
+									 Sport = sport.Name
+								 }).ToList();
+
+					return users;
+				}
+			}
+			catch (Exception ex)
+			{
+				throw new Exception(ex.Message);
+			}
+		}
+
+		/// <summary>
+		/// Get all articles by filters.
+		/// </summary>
+		/// <param name="typeSport">Name type sport.</param>
+		/// <returns>List articles.</returns>
+		[HttpGet("Sport={TypeSport}")]
+		public IEnumerable<object> Get([FromRoute(Name = "TypeSport")] string typeSport)
+		{
+			try
+			{
+				using (SportNewsContext db = new SportNewsContext())
+				{
+					var users = (from article in db.Article
+								 join user in db.User on article.AuthorId equals user.Id
+								 join sport in db.TypeSport on article.TypeSportId equals sport.Id
+								 where sport.Name.ToLower().IndexOf(typeSport.ToLower()) != -1
+								 select new
+								 {
+									 Id = article.Id,
+									 Header = article.Header,
+									 ShortText = article.ShortText,
+									 Text = article.Text,
+									 ImageUrl = article.ImageUrl,
+									 Author = user.Surname + user.Name[0] + user.Patronymic[0],
+									 DatePublish = article.DatePublish,
+									 Sport = sport.Name
 								 }).ToList();
 
 					return users;
